4c5b99d4bbb2b4db124c39091765f15afdeb689a
Mr.Owl <tianj_zhaoyingru@sina.cn>
2024-01-30 23:35:58 +0800

    commit

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1706628259',resources: [
+    cacheName: 'chirpy-1706628930',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
